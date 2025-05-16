ef7d1e9c9137ae61481875516fa391cbbb5e970b
Rui Guerreiro <rui.guerreiro@mindera.com>
2025-05-16 15:45:58 +0100

    WIP PDP Tests

---
+++ screens/ios/pdpScreen/pdpScreen.js
@@ -0,0 +1,11 @@
+output.pdpScreen = {
+//    sizeSelector: 'size-selector',
+    addToBagButton: 'Add to bag',
+    addToWishlistButton: 'Add to wishlist',
+//    addedToBagMessage: 'added-to-bag-message',
+//    productImage: 'product-image',
+//    productTitle: 'product-title',
+    productPrice: 'product-price-full',
+    productColour: 'Colour:',
+    productSize: 'Size:'
+} 
