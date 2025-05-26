7991a4ed2aff5cc1dd414b59800b26f0778bc356
Aires Oliveira <aires.oliveira@mindera.com>
2025-05-26 09:40:33 +0100

    add other framework

---
+++ screens/android/searchScreen/searchScreen.js
@@ -0,0 +1,16 @@
+// searchScreen.js
+output.searchScreen = {
+    textToSearch: 'search-input|searchInput|search_input|searchbox|search-box',
+    backButton: 'back-btn|backBtn|back_btn|back-button|backButton',
+    magnifier: 'magnifyingglass|search-icon|searchIcon',	
+    closeButton: 'close-btn|closeBtn|close_btn|close-button|closeButton',
+    recenteSearchTitle: 'recent-search-title|recentSearchTitle',
+    clearRecentSearches: 'clear|clearAll|clear-all|clearHistory',
+    recentSearchItem: 'recent-search-item|recentSearchItem',
+    removeEntryButton: 'close-btn|closeBtn|remove-btn|removeBtn',
+    searchSuggestionsTitle: 'search-suggestions-title|searchSuggestionsTitle',
+    suggestionsItem: 'suggestions-item|suggestionsItem',
+    moreProductsCta: 'more-products-cta|moreProductsCta',
+    brandsListLink: 'brands-list-link|brandsListLink',
+    emptyScreen: 'empty-screen|emptyScreen|no-results|noResults',
+}
---
+++ screens/iOS/searchScreen/searchScreen.js
@@ -0,0 +1,16 @@
+// searchScreen.js
+output.searchScreen = {
+    textToSearch: 'search-input',
+    backButton: 'back-btn',
+    magnifier: 'magnifyingglass',	
+    closeButton: 'close-btn',
+    recenteSearchTitle: 'recent-search-title',
+    clearRecentSearches: 'clear',
+    recentSearchItem: 'recent-search-item',
+    removeEntryButton: 'close-btn',
+    searchSuggestionsTitle: 'search-suggestions-title',
+    suggestionsItem: 'suggestions-item',
+    moreProductsCta: 'more-products-cta',
+    brandsListLink: 'brands-list-link',
+    emptyScreen: 'empty-screen',
+}
